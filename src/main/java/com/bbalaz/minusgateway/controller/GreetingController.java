8e9294ac0c4861f37049ab7f2211601e7ae06031
Balazinski <bartosz.balazinski@nuance.com>
2022-03-09 16:47:34 -0500

    new stuff

--- src/main/java/com/bbalaz/minusgateway/controller/GreetingController.java
+++ src/main/java/com/bbalaz/minusgateway/controller/GreetingController.java
@@ -3,6 +3,7 @@ package com.bbalaz.minusgateway.controller;
 import com.bbalaz.minusgateway.message.Greeting;
 import com.bbalaz.minusgateway.message.Hello;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.kafka.StreamsBuilderFactoryBeanCustomizer;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -10,6 +11,9 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.util.HtmlUtils;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 @Controller
 @Log4j2
@@ -28,6 +32,24 @@ public class GreetingController {
     public void run() {
         log.info("Running experimentator");
 
+        Integer[] array = new Integer[] { 1,2,3,4 };
+
+        log.info("Array " + Arrays.toString(array));
+
+        Stream<Integer> stream = Stream.of(array);
+
+        StringBuilder sb = new StringBuilder();
+
+        stream.forEach( e -> sb.append(e).append(", "));
+
+        log.info("Stream before: "+ sb.toString());
+
+        stream.forEach( e-> e = e*10 );
+
+        sb = new StringBuilder();
+        stream.forEach( e -> sb.append(e).append(", "));
+
+        log.info("Stream after " + stream.toString());
     }
 
 }
